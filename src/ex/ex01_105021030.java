1309deec8f51383fd04ba4bb5f31266899d2a0c2
battlesushi <105021030@live.asia.edu.tw>
2016-09-26 16:45:32 +0800

    ex01

--- src/ex/ex01_105021030.java
+++ src/ex/ex01_105021030.java
@@ -1,7 +1,7 @@
 package ex;
 import java.util.*;
 /*
- * Topic: 電話公司的計費方式是 600分鐘以下每分鐘0.5元；
+ * Topic: 電話公司的計費方式是 600分鐘以下 	[每分鐘0.5元]
  * 	600~1200分鐘電話費以9折計算；1200分鐘以上電話費以79折計算，
  *  讓使用者輸入單月使用分時間(分鐘)，算出電話費(元)
  * Date: 2016/09/26
@@ -18,10 +18,10 @@ public class ex01_105021030 {
 		}
 		else{
 			if(minute>600&&minute<=1200){
-				System.out.print("電話費:"+minute*0.5*0.9);	
+				System.out.print("電話費:"+minute*price*0.9);	
 			}
 			else{
-				System.out.print("電話費:"+minute*0.5*0.79);
+				System.out.print("電話費:"+minute*price*0.79);
 			}
 		}
 	
